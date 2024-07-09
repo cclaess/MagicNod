a6a6271f5b5732895f5b2f1f419d8695bef95710
Cris Claessens <cclaessens3@int5.local.snellius.surf.nl>
2024-07-09 13:25:59 +0200

    gitignore file added

--- scripts/train_instructpix2pix.sh
+++ scripts/train_instructpix2pix.sh
@@ -1,8 +1,15 @@
 export MODEL_NAME="stabilityai/stable-diffusion-2-1"
-export OUTPUT_DIR="./../models/InstructPix2Pix/"
-export DATASET_PATH="./../../../Datasets/LIDC-IDRI-Processed-GenAI/"
+export OUTPUT_DIR=/gpfs/work4/0/tese0618/Projects/MagicNod/models/InstructPix2Pix/
+export DATASET_PATH="/gpfs/work4/0/tese0618/Projects/MagicNod/dataset/"
 
-cd "./../generativezoo/models/SD"
+export WANDB_API_KEY="9c9ca3cd0f56b3e1ec268da8b0b8d545fbe85946"
+export WANDB_DIR=/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/
+export WANDB_CONFIG_DIR=/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/
+export WANDB_CACHE_DIR=/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/
+export WANDB_START_METHOD="thread"
+wandb login
+
+cd "/gpfs/work4/0/tese0618/Projects/MagicNod/generativezoo/models/SD"
 
 accelerate launch --mixed_precision="fp16" InstructPix2Pix.py \
     --pretrained_model_name_or_path=$MODEL_NAME \
@@ -14,10 +21,11 @@ accelerate launch --mixed_precision="fp16" InstructPix2Pix.py \
     --checkpointing_steps=5000 \
     --learning_rate=5e-05 --max_grad_norm=1 --lr_warmup_steps=0 \
     --conditioning_dropout_prob=0.05 \
-    --validation_image "./../../../../../Datasets/LIDC-IDRI-Processed-GenAI/LIDC-IDRI-0001/01-01-2000-NA-NA-30178/Masked/3000566.000000-NA-03192_90.png" \
-    --validation_prompt "fill the red squares with a pulmonary nodule" \
+    --validation_image=/gpfs/work4/0/tese0618/Projects/MagicNod/dataset/original_images/LIDC-IDRI-0001_01-01-2000-NA-NA-30178_3000566.000000-NA-03192_90.png \
+    --validation_prompt="fill the red squares with pulmonary nodules" \
     --seed=42 \
-    --report_to=wandb \
+    --report_to="wandb" \
     --original_image_colum="original_image" \
     --edited_image_column="edited_image" \
-    --edit_prompt_column="edit_prompt"
+    --edit_prompt_column="edit_prompt" \
+    --dataloader_num_workers=10
