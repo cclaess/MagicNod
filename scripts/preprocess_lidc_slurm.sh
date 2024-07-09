8df72cf86c914934d2f8292aa74a91cb5d3ef71e
Cris Claessens <cclaessens3@int6.local.snellius.surf.nl>
2024-07-09 09:21:34 +0200

    Add preprocessing script for SLURM

---
+++ scripts/preprocess_lidc_slurm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+#SBATCH --time=6:00:00
+
+cd "/gpfs/work4/0/tese0618/Projects/MagicNod"
+
+srun apptainer exec --nv "/gpfs/work4/0/tese0618/Containers/misc_v6.sif" python3 scripts/preprocess_lidc_gen.py --input_dir "/gpfs/work4/0/tese0618/Datasets/LIDC-IDRI-Processed" --output_dir "/gpfs/work4/0/tese0618/Datasets/LIDC-IDRI-Processed-GenAI"
