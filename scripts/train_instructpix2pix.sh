6c7d24d3369413d92c1ddbe0da1e6d5e78e1fb8e
cclaess <c.h.b.claessens@student.tue.nl>
2024-07-11 11:12:21 +0200

    relative paths

--- scripts/train_instructpix2pix.sh
+++ scripts/train_instructpix2pix.sh
@@ -3,9 +3,9 @@ export OUTPUT_DIR=/gpfs/work4/0/tese0618/Projects/MagicNod/models/InstructPix2Pi
 export DATASET_PATH="./../../../dataset/"
 
 export WANDB_API_KEY="9c9ca3cd0f56b3e1ec268da8b0b8d545fbe85946"
-export WANDB_DIR=/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/
-export WANDB_CONFIG_DIR=/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/
-export WANDB_CACHE_DIR=/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/
+export WANDB_DIR="/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/"
+export WANDB_CONFIG_DIR="/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/"
+export WANDB_CACHE_DIR="/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/"
 export WANDB_START_METHOD="thread"
 wandb login
 
@@ -21,13 +21,8 @@ accelerate launch --mixed_precision="fp16" InstructPix2Pix.py \
     --checkpointing_steps=5000 \
     --learning_rate=5e-05 --max_grad_norm=1 --lr_warmup_steps=0 \
     --conditioning_dropout_prob=0.05 \
-<<<<<<< HEAD
-    --validation_image=/gpfs/work4/0/tese0618/Projects/MagicNod/dataset/original_images/LIDC-IDRI-0001_01-01-2000-NA-NA-30178_3000566.000000-NA-03192_90.png \
+    --validation_image="./../../../dataset/original_images/LIDC-IDRI-0001_01-01-2000-NA-NA-30178_3000566.000000-NA-03192_90.png" \
     --validation_prompt="put a pulmonary nodule in the red squares" \
-=======
-    --validation_image="/gpfs/work4/0/tese0618/Projects/MagicNod/dataset/original_images/LIDC-IDRI-0001_01-01-2000-NA-NA-30178_3000566.000000-NA-03192_90.png" \
-    --validation_prompt="fill the red squares with pulmonary nodules" \
->>>>>>> 18fd569679deaab8a4c617034367a907f50810b8
     --seed=42 \
     --report_to="wandb" \
     --original_image_colum="original_image" \
