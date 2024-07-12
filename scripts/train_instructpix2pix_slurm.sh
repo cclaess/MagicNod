10af4d3be768d381b92d601efe40cb3b4371f43b
cclaess <c.h.b.claessens@student.tue.nl>
2024-07-12 16:38:01 +0200

    Update for second training

--- scripts/train_instructpix2pix_slurm.sh
+++ scripts/train_instructpix2pix_slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=72
 #SBATCH --gpus=4
 #SBATCH --partition=gpu
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 
 
 mkdir -p "/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/"
