7e1929ee6cc98692b7c3cbb9e455b251ce401658
cclaess <c.h.b.claessens@student.tue.nl>
2024-07-16 11:34:23 +0200

    Minor bug fix

--- scripts/preprocess_lidc_new.py
+++ scripts/preprocess_lidc_new.py
@@ -101,7 +101,6 @@ def main(args):
             # create a mask with the bounding box of the nodule
             slice_msk_bbox = sitk.Image(size, sitk.sitkUInt8)
             slice_msk_bbox[bbox[0]:bbox[0] + bbox[2], bbox[1]:bbox[1] + bbox[3]] = 1
-            slice_msk_bbox.CopyInformation(sitk_img)
 
             # loop over the slices of the nodule and save the masked and unmasked images
             for z in range(z_start, z_end):
@@ -109,6 +108,7 @@ def main(args):
                 # get the axial slice of the image and mask it
                 slice_img = sitk_img[:, :, z]
                 slice_msk = sitk_msk[:, :, z]
+                slice_msk_bbox.CopyInformation(slice_img)
                 slice_img_rgb_masked = create_rgb_image_with_mask(slice_img, slice_msk_bbox)	
 
                 # Rescale the mask for saving
