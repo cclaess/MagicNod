94b1d8c45d8a4e6e48425fd9f1643f3fa1e9374e
cclaess <c.h.b.claessens@tue.nl>
2024-07-28 19:10:35 +0200

    Increase time cap

--- scripts/train_inpainting_unet_slurm.sh
+++ scripts/train_inpainting_unet_slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_h100
-#SBATCH --time=72:00:00
+#SBATCH --time=96:00:00
 
 
 mkdir -p "/gpfs/work4/0/tese0618/Projects/MagicNod/wandb/$SLURM_JOBID/"
