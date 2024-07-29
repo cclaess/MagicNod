47cc757720dfbfd8d383f7fc6fc04a91b2f6e9e0
cclaess <c.h.b.claessens@student.tue.nl>
2024-07-29 13:52:31 +0200

    Draw bboxes on top

--- experiments/inference_instructpix2pix.py
+++ experiments/inference_instructpix2pix.py
@@ -3,7 +3,10 @@ import argparse
 from glob import glob
 from pathlib import Path
 
+import cv2
 import torch
+import numpy as np
+from PIL import Image
 from diffusers import StableDiffusionInstructPix2PixPipeline
 from diffusers.utils import load_image
 
@@ -32,11 +35,13 @@ def main(args):
     pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained(args.model_path, torch_dtype=torch.float16).to("cuda")
     generator = torch.Generator("cuda").manual_seed(args.seed)
 
-    image_paths = glob(os.path.join(args.data_path, '**', 'Masked', '*.png'), recursive=True)
+    image_paths = sorted(glob(os.path.join(args.data_path, '**', 'Masked', '*.png'), recursive=True))
+    mask_paths = sorted(glob(os.path.join(args.data_path, '**', 'Mask', '*.png'), recursive=True))
 
-    for image_path in image_paths:
+    for image_path, mask_path in zip(image_paths, mask_paths):
 
         image = load_image(image_path)
+        mask = load_image(mask_path)
 
         edited_image = pipeline(
         args.prompt,
@@ -46,6 +51,22 @@ def main(args):
         guidance_scale=args.guidance_scale,
         generator=generator,
         ).images[0]
+
+        # retreive bounding boxes of the nodules using the mask where mask > 0
+        mask_array = np.array(mask)
+        _, binary_mask = cv2.threshold(mask_array, 0, 255, cv2.THRESH_BINARY)
+        num_labels, labels_im = cv2.connectedComponents(binary_mask)
+        bboxes = []
+        for i in range(1, num_labels):
+            component_mask = (labels_im == i).astype(np.uint8)
+            x, y, w, h = cv2.boundingRect(component_mask)
+            bboxes.append((x, y, w, h))
+        
+        # draw bounding boxes on the edited image
+        edited_image = np.array(edited_image)
+        for x, y, w, h in bboxes:
+            cv2.rectangle(edited_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        edited_image = Image.fromarray(edited_image)
 
         save_path = Path(image_path.replace(args.data_path, args.output_path))
         save_path.parent.mkdir(parents=True, exist_ok=True)
