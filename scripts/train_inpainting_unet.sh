cbc5515c158c08aa8495fc5ca42fb775e0ecc1e3
cclaess <c.h.b.claessens@student.tue.nl>
2024-11-12 09:36:07 +0100

    Fix OOM error

--- scripts/train_inpainting_unet.sh
+++ scripts/train_inpainting_unet.sh
@@ -7,7 +7,7 @@ accelerate launch --multi-gpu --mixed_precision "fp16" \
     experiments/train_inpainting_unet.py \
     --experiment-name "test_run_lidc" \
     --data-dir "./data/LIDC-IDRI" \
-    --batch-size 32 \
+    --batch-size 16 \
     --epochs 100 \
     --lr 0.0001 \
     --num-workers 10 \
