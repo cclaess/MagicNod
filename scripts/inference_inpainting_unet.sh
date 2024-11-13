7d17e06b64df02f4714d818aec00a606b913edf5
cclaess <c.h.b.claessens@student.tue.nl>
2024-11-13 14:56:17 +0100

    Bug fix

--- scripts/inference_inpainting_unet.sh
+++ scripts/inference_inpainting_unet.sh
@@ -3,7 +3,7 @@ cd ~/Projects/MagicNod | exit 1
 mkdir -p $WANDB_DIR
 wandb login
 
-python3 experiments/train_inpainting_unet.py \
+python3 experiments/inference_inpainting_unet.py \
     --model-path "./checkpoints/test_run_lidc/best_model.pth" \
     --data-dir "./data/LIDC-IDRI" \
     --output-dir "./results/LIDC-IDRI/inpainting" \
