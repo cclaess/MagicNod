c528823298cf25dfab2f1f64c3dd09e30743da0c
cclaess <c.h.b.claessens@student.tue.nl>
2024-11-14 13:13:15 +0100

    Edit model path

--- scripts/inference_inpainting_unet.sh
+++ scripts/inference_inpainting_unet.sh
@@ -4,6 +4,6 @@ mkdir -p $WANDB_DIR
 wandb login
 
 python3 experiments/inference_inpainting_unet.py \
-    --model-path "./checkpoints/test_run_lidc/best_model.pth" \
+    --model-path "./checkpoints/test_run_lidc_2/best_model.pth" \
     --data-dir "./data/LIDC-IDRI" \
     --output-dir "./results/LIDC-IDRI/inpainting" \
