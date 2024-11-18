952932c7f7c3ee51f13c0d77e0190fd38031ffef
cclaess <c.h.b.claessens@student.tue.nl>
2024-11-18 15:10:34 +0100

    Upscale experiment

--- scripts/train_inpainting_vqvae.sh
+++ scripts/train_inpainting_vqvae.sh
@@ -8,7 +8,7 @@ accelerate launch --multi-gpu \
     --experiment-name "test_run_lidc_1" \
     --data-dir "./data/LIDC-IDRI" \
     --batch-size 4 \
-    --epochs 100 \
+    --epochs 500 \
     --lr-g 1e-7 \
     --lr-d 5e-7 \
     --num-workers 10 \
