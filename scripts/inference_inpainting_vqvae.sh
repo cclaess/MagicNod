24d8668b2e8522c8fbbbd232c5d7fc8e715c0d26
cclaess <c.h.b.claessens@student.tue.nl>
2024-11-21 09:23:34 +0100

    Add scripts for running inference on VQVAE

---
+++ scripts/inference_inpainting_vqvae.sh
@@ -0,0 +1,12 @@
+cd ~/Projects/MagicNod | exit 1
+
+mkdir -p $WANDB_DIR
+wandb login
+
+python3 experiments/inference_inpainting_vqvae.py \
+    --model-path "./checkpoints/test_run_lidc_4/best_model.pth" \
+    --data-dir "./data/LIDC-IDRI" \
+    --output-dir "./results/LIDC-IDRI/inpainting" \
+    --batch-size 4 \
+    --num-workers 10 \
+    --seed 42
