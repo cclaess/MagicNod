f8c972aec573e0e48bb741e0238dfe851e3be49d
cclaess <c.h.b.claessens@student.tue.nl>
2024-11-28 11:14:31 +0100

    Change checkpoint

--- scripts/inference_inpainting_vqvae.sh
+++ scripts/inference_inpainting_vqvae.sh
@@ -4,7 +4,7 @@ mkdir -p $WANDB_DIR
 wandb login
 
 python3 -u experiments/inference_inpainting_vqvae.py \
-    --model-path "./checkpoints/test_run_lidc_4/best_model.pth" \
+    --model-path "./checkpoints/test_run_lidc_5/final_model.pth" \
     --data-dir "./data/LIDC-IDRI" \
     --output-dir "./results/LIDC-IDRI/inpainting" \
     --batch-size 4 \
