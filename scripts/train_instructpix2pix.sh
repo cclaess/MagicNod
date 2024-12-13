939a7e4f2f72b0f45b95ca56a5a36c8ef2d704b8
cclaess <c.h.b.claessens@tue.nl>
2024-12-13 13:13:42 +0100

    Update training parameters

--- scripts/train_instructpix2pix.sh
+++ scripts/train_instructpix2pix.sh
@@ -8,12 +8,12 @@ accelerate launch --mixed_precision="fp16" generativezoo/models/SD/InstructPix2P
     --train_data_dir="/gpfs/work4/0/tese0618/Projects/MagicNod/data/LIDC-IDRI-pix2pix/" \
     --output_dir="/gpfs/work4/0/tese0618/Projects/MagicNod/checkpoints/instruct_pix2pix_1" \
     --resolution=512 --random_flip \
-    --train_batch_size=32 --gradient_accumulation_steps=1 --gradient_checkpointing \
-    --max_train_steps=25000 \
+    --train_batch_size=128 --gradient_accumulation_steps=1 --gradient_checkpointing \
+    --num_train_epochs=100 \
     --checkpointing_steps=5000 \
-    --learning_rate=5e-05 --max_grad_norm=1 --lr_warmup_steps=0 \
+    --learning_rate=5e-05 --lr_scheduler="cosine" --max_grad_norm=1 --lr_warmup_steps=0 \
     --conditioning_dropout_prob=0.05 \
-    --validation_image="/gpfs/work4/0/tese0618/Projects/MagicNod/results/LIDC-IDRI/inpainting/valid/LIDC-IDRI-0007/01-01-2000-NA-NA-81781/3000631.000000-NA-57680/combined_mask_slice=0007_nod=1.png" \
+    --validation_image="/gpfs/work4/0/tese0618/Projects/MagicNod/results/LIDC-IDRI/inpainting/valid/LIDC-IDRI-0026/01-01-2000-NA-NA-02665/3000519.000000-NA-40086/combined_mask_slice=0000_nod=1.png" \
     --validation_prompt="Put a malignant pulmonary nodule in the masked region." \
     --seed=42 \
     --report_to="wandb" \
