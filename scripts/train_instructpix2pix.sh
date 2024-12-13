8c4a58229705293a90659f517293b140171a09b2
cclaess <c.h.b.claessens@student.tue.nl>
2024-12-13 17:48:34 +0100

    New experiment

--- scripts/train_instructpix2pix.sh
+++ scripts/train_instructpix2pix.sh
@@ -6,10 +6,10 @@ wandb login
 accelerate launch --mixed_precision="fp16" generativezoo/models/SD/InstructPix2Pix.py \
     --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1" \
     --train_data_dir="/gpfs/work4/0/tese0618/Projects/MagicNod/data/LIDC-IDRI-pix2pix/" \
-    --output_dir="/gpfs/work4/0/tese0618/Projects/MagicNod/checkpoints/instruct_pix2pix_1" \
+    --output_dir="/gpfs/work4/0/tese0618/Projects/MagicNod/checkpoints/instruct_pix2pix_2" \
     --resolution=512 --random_flip \
     --train_batch_size=128 --gradient_accumulation_steps=1 --gradient_checkpointing \
-    --num_train_epochs=100 \
+    --num_train_epochs=500 \
     --checkpointing_steps=5000 \
     --learning_rate=5e-05 --lr_scheduler="cosine" --max_grad_norm=1 --lr_warmup_steps=0 \
     --conditioning_dropout_prob=0.05 \
