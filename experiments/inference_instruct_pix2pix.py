ff6f1144fb9b48365455b3550f06191f79b46280
cclaess <c.h.b.claessens@tue.nl>
2024-12-16 11:22:41 +0100

    Add inference logic for Instruct Pix2Pix

--- experiments/inference_instruct_pix2pix.py
+++ experiments/inference_instruct_pix2pix.py
@@ -9,6 +9,7 @@ import numpy as np
 from PIL import Image
 from diffusers import StableDiffusionInstructPix2PixPipeline
 from diffusers.utils import load_image
+# from lungmask import LMInferer
 
 
 def get_args_parser():
@@ -18,12 +19,12 @@ def get_args_parser():
     parser.add_argument('--model_path', type=str, help='Path to the model')
 
     parser.add_argument('--prompt', type=str, help='Prompt for the model')
-    parser.add_argument('--data_path', type=str, help='Path to the images folder')
+    parser.add_argument('--data_dir', type=str, help='Path to the images folder')
     parser.add_argument('--num_inference_steps', type=int, default=50, help='Number of inference steps')
     parser.add_argument('--image_guidance_scale', type=float, default=2.5, help='Image guidance scale')
     parser.add_argument('--guidance_scale', type=float, default=7.5, help='Guidance scale')
 
-    parser.add_argument('--output_path', type=str, help='Path to the output folder')
+    parser.add_argument('--output_dir', type=str, help='Path to the output folder')
     parser.add_argument('--seed', type=int, default=42, help='Random seed')
 
 
@@ -35,8 +36,16 @@ def main(args):
     pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained(args.model_path, torch_dtype=torch.float16).to("cuda")
     generator = torch.Generator("cuda").manual_seed(args.seed)
 
-    image_paths = sorted(glob(os.path.join(args.data_path, '**', 'Masked', '*.png'), recursive=True))
-    mask_paths = sorted(glob(os.path.join(args.data_path, '**', 'Mask', '*.png'), recursive=True))
+    data_dir = Path(args.data_dir)
+
+    image_paths = sorted(glob(
+        str(data_dir / "valid" / "**" / "combined_mask_slice=*_nod=*.png"),
+        recursive=True,
+    ))
+    mask_paths = sorted(glob(
+        str(data_dir / "valid" / "**" / "mask_slice=*_nod=*.png"),
+        recursive=True,
+    ))
 
     for image_path, mask_path in zip(image_paths, mask_paths):
 
@@ -54,23 +63,21 @@ def main(args):
 
         # retreive bounding boxes of the nodules using the mask where mask > 0
         mask_array = cv2.cvtColor(np.array(mask), cv2.COLOR_RGB2GRAY)
-        print(mask_array.shape)
         _, binary_mask = cv2.threshold(mask_array, 0, 255, cv2.THRESH_BINARY)
-        print(binary_mask.shape)
-        num_labels, labels_im = cv2.connectedComponents(binary_mask)
-        bboxes = []
-        for i in range(1, num_labels):
-            component_mask = (labels_im == i).astype(np.uint8)
-            x, y, w, h = cv2.boundingRect(component_mask)
-            bboxes.append((x, y, w, h))
+        x, y, w, h = cv2.boundingRect(binary_mask)
+
+        # Change bbox to a square with the same middle point
+        max_side = max(w, h)
+        x = x + w // 2 - max_side // 2
+        y = y + h // 2 - max_side // 2
+        w = h = max_side
         
         # draw bounding boxes on the edited image
         edited_image = np.array(edited_image)
-        for x, y, w, h in bboxes:
-            cv2.rectangle(edited_image, (x - 5, y - 5), (x + w + 5, y + h + 5), (0, 255, 0), 1)
+        cv2.rectangle(edited_image, (x - 5, y - 5), (x + w + 5, y + h + 5), (0, 255, 0), 1)
         edited_image = Image.fromarray(edited_image)
 
-        save_path = Path(image_path.replace(args.data_path, args.output_path))
+        save_path = Path(image_path.replace(args.data_dir, args.output_dir))
         save_path.parent.mkdir(parents=True, exist_ok=True)
         edited_image.save(save_path)
 
