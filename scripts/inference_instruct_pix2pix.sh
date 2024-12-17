cc125877747f4a1ecf986ab34254f7a58b9de442
cclaess <c.h.b.claessens@student.tue.nl>
2024-12-17 08:45:09 +0100

    Generate malignant nodules

--- scripts/inference_instruct_pix2pix.sh
+++ scripts/inference_instruct_pix2pix.sh
@@ -5,7 +5,7 @@ wandb login
 
 python3 -u experiments/inference_instruct_pix2pix.py \
     --model-path "./checkpoints/instruct_pix2pix_2/" \
-    --prompt "Put a benign lung nodule in the masked region" \
+    --prompt "Put a malignant lung nodule in the masked region" \
     --data-dir "./results/LIDC-IDRI/inpainting/" \
-    --output-dir "./results/LIDC-IDRI/pix2pix-benign-increased-guidance-2" \
+    --output-dir "./results/LIDC-IDRI/pix2pix-malignant-increased-guidance-2" \
     --guidance-scale 50.0 \
