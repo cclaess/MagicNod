838e52f056467779cfde42a9482f4fecc644bffe
cclaess <c.h.b.claessens@student.tue.nl>
2024-12-17 08:17:47 +0100

    Increase guidance scale

--- scripts/inference_instruct_pix2pix.sh
+++ scripts/inference_instruct_pix2pix.sh
@@ -7,4 +7,5 @@ python3 -u experiments/inference_instruct_pix2pix.py \
     --model-path "./checkpoints/instruct_pix2pix_2/" \
     --prompt "Put a benign lung nodule in the masked region" \
     --data-dir "./results/LIDC-IDRI/inpainting/" \
-    --output-dir "./results/LIDC-IDRI/pix2pix-benign" \
+    --output-dir "./results/LIDC-IDRI/pix2pix-benign-increased-guidance" \
+    --guidance-scale 15.0 \
