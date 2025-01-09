81e2f72da7eebaaf41eb51933c1f0cb48c1d10e7
cclaess <c.h.b.claessens@student.tue.nl>
2025-01-09 13:43:04 +0100

    Change output folder

--- scripts/nodule_synthesis.sh
+++ scripts/nodule_synthesis.sh
@@ -7,4 +7,4 @@ python3 -u experiments/nodule_synthesis.py \
     --model-path-vqvae "./checkpoints/test_run_lidc_7/best_model.pth" \
     --model-path-pix2pix "./checkpoints/instruct_pix2pix_2/" \
     --data-dir "./data/LNDb" \
-    --output-dir "./results/LNDb/nodule_synthesis" \
+    --output-dir "./results/LNDb/nodule_synthesis_2" \
